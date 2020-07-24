761cc5c235750a25ec61d262901e42bb2ef4dce4
Hans <twd2206@gmail.com>
2020-07-24 09:55:15 -0500

    fixed

--- app/Models/Hooks/StaffHook.js
+++ app/Models/Hooks/StaffHook.js
@@ -3,12 +3,13 @@
 const Perfil = use('App/Models/PerfilLaboral');
 const slugify = require('slugify')
 const StaffRequirement = use('App/Models/StaffRequirement');
+const uid = require('uid');
 
 const StaffHook = exports = module.exports = {}
 
 StaffHook.generateSlug = async (staff) => {
     let perfil = await Perfil.find(staff.perfil_laboral_id);
     if (perfil) {
-        staff.slug = slugify(`${perfil.nombre} ${staff.convocatoria_id}`, { lower: true });
+        staff.slug = slugify(`${perfil.nombre} ${staff.convocatoria_id} ${uid()}`, { lower: true });
     }
 }
